323ad38bd31db88327ed81cbd196fa1880eddecd
MrAbraham <abraaopessoa@yahoo.com.br>
2021-12-16 01:10:08 -0300

    Initial Commit

---
+++ alura-viagens/alura_viagensApp.swift
@@ -0,0 +1,17 @@
+//
+//  alura_viagensApp.swift
+//  alura-viagens
+//
+//  Created by Abraao Barros on 16/12/21.
+//
+
+import SwiftUI
+
+@main
+struct alura_viagensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
